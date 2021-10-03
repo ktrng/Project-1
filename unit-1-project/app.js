0cc73d24cafd931e376bab5966141171a8455054
Kevin Trang <ktrngcode@gmail.com>
2021-10-02 22:08:44 -0400

    added modal and opening/closing buttons

--- unit-1-project/app.js
+++ unit-1-project/app.js
@@ -1,14 +1,29 @@
 $(() => {
+
+
+
   $.ajax(
     {
       url: 'https://swapi.dev/api/films/'
     }
   ) .then(
     (data) => {
+      const openModal = () => {
+        $('.modal').css('display', 'block')
+      }
+      const closeModal = () => {
+        $('.modal').css('display', 'none')
+      }
+
       console.log(data);
+      $('#button-1').on('click', openModal)
+      $('#close-button').on('click', closeModal)
+
     },
     () => {
     console.log('bad request');
     }
   )
+
+
 })
